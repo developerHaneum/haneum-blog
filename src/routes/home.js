76f5d8cb1660a085acc435b31c03f44193137aeb
Cha Haneum <developer_haneum_11@icloud.com>
2022-02-17 20:06:16 +0900

    Remove before function Enhance WA

--- src/routes/home.js
+++ src/routes/home.js
@@ -13,11 +13,8 @@ const contentMsg = `
     <p>If you are curious about my web application or information, please check <a href="https://github.com/developerHaneum">GitHub  </a> or check out <a href="/menu">more posts</a> on my blog.</p>
     <p>Also, to check more blog posts, click <a href="/menu">the Menu link</a> at the top or access <a href="/menu">/menu</a>.</p>
     <br/>
-    <details>
-        <summary>Attention</summary>
-        <p>This web application doesn't support IE browser.</p>
-        <p>And depending on the user's environment, the web application may be unstable.</p>
-    </details>
+    <p>Attention, This web application doesn't support IE browser.</p>
+    <p>And depending on the user's environment, the web application may be unstable.</p>
     <br/>
     <p>MIT License &copy; ${new Date().getFullYear()} Cha Haneum</p>
 `;
