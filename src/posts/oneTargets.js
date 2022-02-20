3d476f6f112a436db583b26d3191ac4aa3ee01e4
Cha Haneum <developer_haneum_11@icloud.com>
2022-02-20 17:12:49 +0900

    Edit styles

--- src/posts/oneTargets.js
+++ src/posts/oneTargets.js
@@ -3,7 +3,7 @@ import { postMaker } from '../components/postMaker.js';
 const contentMsg = `
     <h1>2021 목표</h1>
     <ul>
-        <li class="second-list"><p>이제 돈좀 벌어봅시다! (Let's make some money!)</p></li>
+        <li class="second-list"><p>평균 2등급 맞기.</p></li>
         <li class="second-list"><p>Airpods Pro 사기.</p></li>
         <li class="second-list"><p>자체 플렛폼으로 MAU 100명 달성하기.</p></li>
         <li class="second-list"><p class="strike">Vanilla JS + Express.js로 SPA 개인 블로그 만들기.</p></li>
