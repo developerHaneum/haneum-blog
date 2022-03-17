c466707fc4330ef5b3ccc767fe64f83ac0382688
Cha Haneum <developer_haneum_11@icloud.com>
2022-03-17 16:05:27 +0900

    whole fix

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -1,35 +1,8 @@
 import { postMaker } from '../components/postMaker.js';
+import { map } from 'fxjs';
 
-const korean = '(Korean)';
 const contentMsg = `
-    <h1>Blog posts</h1>
-    <ul>
-        <li class="second-list"><h2>Personal posts</h2></li>
-        <ul>
-            <li class="third-list">
-                <p><a href="/">Hi, Nice to meet you.</a></p>
-                <p><a href="/devil-putin">Devil Putin. ${korean}</a></p>
-                <p><a href="/starting-school">Starting School. ${korean}</a></p>
-            </li>
-        </ul>
-    </ul>
-    <br/>
-    <ul>
-        <li class="second-list"><h2>Git related posts</h2></li>
-        <ul>
-            <li class="third-list">
-                <p><a href="/git-commit-msg-rules">Git commit message rules ${korean}</a></p>
-            </li>
-        </ul>
-    </ul>
-    <br/>
-    <ul>
-        <li class="second-list"><h2>Functional programming related posts</h2></li>
-        <ul>
-            <li class="third-list">
-                <p><a href="/fp-basics">Basics ${korean}</a></p>
-            </li>
-        </ul>
-    </ul>
+  <a href="/fp-basics">Fp bacis</a>
 `;
+
 export const menu = () => postMaker(contentMsg);
