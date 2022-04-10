8807927a00235085f3c09929ee268ff51380d730
Cha Haneum <developer_haneum_11@icloud.com>
2022-04-10 17:52:06 +0900

    post: add: select post

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -1,16 +1,30 @@
 import { postMaker } from '../components/postMaker.js';
-
+import { routeTitles } from '../components/routeContents.js';
 const contentMsg = `
   <h1>Blog posts</h1>
   <ul>
     <li class="second-list">
-      <h2>Personal posts</h2>
+      <h2>Personal posts</h2> 
       <ul>
         <li class="third-list">
           <p><a href="/">Hi, Nice to meet you.</a></p>
-          <p><a href="/corona-diary">코로나 일지.</a></p>
+          <p><a href="/devil-putin">Devil Putin.</a></p>
+          <p><a href="/love-is">Love is.</a></p>
+          <p><a href="/plan">Plan.</a></p>
+          <p><a href="/march">March.</a></p>
         </li>
       </ul>
   </ul>
 `;
 export const menu = () => postMaker(contentMsg);
+
+// window.onload = () => {
+//   const routes = Object.entries(routeTitles);
+//   console.log(routes.length);
+//   routes.forEach(e => {
+//     const elem = document.createElement('li');
+//     elem.classList.add('.thrid-list');
+//     document.querySelector('ul').appendChild(elem);
+//   });
+//   document.querySelector('.thrid-list').innerHTML = `hle`;
+// };
