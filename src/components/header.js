6abfdd30c27ebd1f194e73c835f367e61ae62fab
Cha Haneum <developer_haneum_11@icloud.com>
2022-04-15 20:31:48 +0900

    feat: whole font size changed px (vh to px)

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,7 @@ import './header.css';
 
 export const header = () => `
     <header class="header">
-        <menu class="header-menu"><a href="/menu">Menu</a></menu>
-        <div class="header-title">
-            <a href="/">Haneum Blog</a>
-        </div>
+        <a class="header-menu" href="/menu">Menu</a>
+        <a class="header-title" href="/">Haneum Blog</a>
     </header>
 `;
