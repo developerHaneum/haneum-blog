8d7412d0c9b0bcf65acfc4124db4ac5e579a5290
Cha Haneum <developer_haneum_11@icloud.com>
2022-05-07 23:59:01 +0900

    feat: SEO removed

--- src/components/renderRoute.js
+++ src/components/renderRoute.js
@@ -3,28 +3,8 @@ import { routeContents } from './routeContents.js';
 export const renderRoute = path => {
   if (!routeContents[path].title) {
     document.title = 'This is Haneum';
-    document
-      .querySelector('meta[property="og:title"]')
-      .setAttribute('content', 'Haneum Blog');
-    document
-      .querySelector('meta[property="og:description"]')
-      .setAttribute('content', 'Haneum Blog');
   } else {
     document.title = routeContents[path].title;
-    document
-      .querySelector('meta[property="og:title"]')
-      .setAttribute('content', `${routeContents[path].title}`);
-    document
-      .querySelector('meta[property="og:description"]')
-      .setAttribute('content', `${routeContents[path].title}`);
   }
-  document
-    .querySelector('meta[property="og:url"]')
-    .setAttribute(
-      'content',
-      `${window.location.protocol}//${window.location.host}${
-        window.location.pathname === '/' ? '' : window.location.pathname
-      }`
-    );
   routeContents[path].route();
 };
