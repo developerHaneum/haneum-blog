08f711a9478a9e3fb149ec27bb031b36572d9b91
Cha Haneum <developer_haneum_11@icloud.com>
2022-05-07 13:41:27 +0900

    post: added

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,15 +3,13 @@ import { postMaker } from '../components/postMaker.js';
 export const home = () => {
   const render = () => {
     const contentMsg = `
-      <p>Hello. My name is Cha Haneum.<br/>
-      I am Korean, I am doing front-end development.</p>
+      <p>My name is Haneum Cha, I am Korean, and I do frontend development.</p>
       <p>I take simple and intuitive web development very seriously.<br/>
-      It has to do with web accessibility and has a huge impact on UX.</p>
-      <p>Also, I dream of a web world where everyone from kindergarteners to the elderly can use.<br/>
-      This is my biggest goal and one of my challenges.</p>
-      <p>If you would like to see many posts from your blog,<br/>
-      you can view more blog posts by clicking on <a href="/feed">the blog post title</a>, by clicking on <a href="/feed">/feed</a> or by typing <a href="/feed">/feed</a> after your domain.</p>
-      <p>If you're curious, check out <a href="https://github.com/chebread">GitHub</a>.</p>
+      It has to do with web accessibility and has a huge impact on UX.<br/>
+      We also dream of a web world that can be used by everyone, from kindergarteners to the elderly.</p>
+      <p>If you would like to see many articles on the blog,<br/>
+      you can view more blog posts by clicking on <a href="/feed">the blog post title</a>.</p>
+      <p>If you're curious about me, check out <a href="https://github.com/chebread">GitHub</a>.</p>
     `;
     const contents = {
       title: 'Hi, Nice to meet you.',
