ced524db01081e04dc8f6142726d293368afdb10
Cha Haneum <developer_haneum_11@icloud.com>
2022-05-15 20:33:59 +0900

    feat: Overall edited

--- src/routes/home.js
+++ src/routes/home.js
@@ -9,8 +9,7 @@ export const home = () => {
       It has to do with web accessibility and has a huge impact on UX.
       We also dream of a web world that can be used by everyone, from kindergarteners to the elderly.
 
-      If you would like to see many articles on the blog,
-      you can view more blog posts by clicking on <a href="/feed">the blog post title</a>.
+      If you would like to see many articles on the blog, you can view more blog posts by clicking on <a href="/feed">the blog post title</a>.
       
       If you're curious about me, check out <a target="_blank" href="https://github.com/chebread">GitHub</a>.
     `;
