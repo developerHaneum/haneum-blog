c5e644e1eeaaed26224d62afddeb9f5b46eae147
Cha Haneum <developer_haneum_11@icloud.com>
2022-05-16 00:18:14 +0900

    fix: bugs fixed

--- src/components/postMaker.js
+++ src/components/postMaker.js
@@ -11,7 +11,11 @@ export const postMaker = contents => {
         'ontouchstart' in document.documentElement ? 'touchstart' : 'mouseover',
         e => {
           if (e.target.parentNode.className === 'content-title') {
-            e.target.style = `color: white; font-size: 27.25px; line-height: 35px;`;
+            e.target.style = `color: white; ${
+              'ontouchstart' in document.documentElement
+                ? 'font-size: 27.25px; line-height: 35px;'
+                : 'font-size: 28.75px; line-height: 35px;'
+            }`;
           } else {
             e.target.style = 'color: rgb(190, 150, 30);';
           }
