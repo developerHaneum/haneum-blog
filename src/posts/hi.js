865798de05ebc907e01756be2d77b33bf002de67
Cha Haneum <developer_haneum_11@icloud.com>
2022-05-22 22:04:56 +0900

    feat: whole edited

--- src/posts/hi.js
+++ src/posts/hi.js
@@ -7,10 +7,6 @@ export const hi = () => {
 
       I take simple and intuitive web development very seriously. It has to do with web accessibility and has a huge impact on UX. We also dream of a web world that can be used by everyone, from kindergarteners to the elderly.
 
-      I am oriented towards functional programming, and I value a deep understanding of basic JavaScript. Since JavaScript is the only programming language in the front-end, I think a deep understanding of JavaScript is most important.
-
-      On my blog, I write articles containing my personal views, and technical articles are written while working as <a href="https://velog.io/@haneum">@haneum</a> in a place called velog.
-
       If you would like to see many articles on the blog, you can view more blog posts by clicking on <a href="/">the blog post title</a>.
         
       Also, if you're curious about me, check out <a href="https://github.com/chebread">GitHub</a>. Also, if you would like to contact me, please contact us by <a href="mailto:chahaneum@naver.com">email</a>.
