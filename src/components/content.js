2caa2177a125898f4c38638c907dd8c54d0b7d00
Cha Haneum <developer_haneum_11@icloud.com>
2022-06-03 22:33:11 +0900

    backup

--- src/components/content.js
+++ src/components/content.js
@@ -4,12 +4,12 @@ import replaceContent from './replaceContent.js';
 const content = contents => `
   <div class="container">
     <div class="container-content">
-      <div class="container-wrapper">
+      <div class="content-wrapper">
         <div class="content-title" role="title">
           <a href="/">${contents.title}</a>
         </div>
       </div>
-      <div class="container-wrapper" role="contents">
+      <div class="content-wrapper" role="contents">
         <div class="content-main">
           ${replaceContent(contents.content)}</div>
         </div>
